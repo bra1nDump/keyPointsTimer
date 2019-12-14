d217631678bbd4dce905057145fdc461c6175922
Kirill Dubovitskiy <kirill2003de@gmail.com>
2019-12-14 00:28:38 -0800

    ^ Problem > Solution V Drawbacks
    ^ Too many interactions on one screen:
      (add/delete point, change time allocated for point
      , start/stop/reset timer)
    > Split into two screens: note taking, presenting
    V
      * Not explaining that each line in the notes will become a point
      * More screens
      * If you go back to notes, time allocation between points gets lost
    
    ^ Hidden features (ex: reorderable points discoverable by long press)
    > Basic text editing during note taking already addresses reordering
    
    ^ Timer too far from timer controls (top to bottom layout):
      timer | presentation points (80% height) | [ start button, stop button ]
    > Controls moved to the top, now using icons instead of text

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,104 @@ import 'dart:ui' as prefix0;
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:flutter/widgets.dart';
+
 void main() => runApp(App());
 
-class App extends StatefulWidget {
-  @override
-  AppState createState() => AppState();
+class App extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: NotesScreen.name,
+      onGenerateRoute: (settings) {
+        if (settings.name == NotesScreen.name) {
+            return MaterialPageRoute(
+              builder: (context) => NotesScreen()
+            );
+        } else if (settings.name == PresentationPage.routeName) {
+            String notes = settings.arguments;
+            var notEmptyLines = notes
+                .split("\n")
+                .where((line) => line.replaceAll(' ', '').isNotEmpty);
+            var presentation = notEmptyLines
+                .map((point) => Point(point, 10))
+                .toList();
+            return MaterialPageRoute(
+              builder: (context) => PresentationPage(presentation)
+            );
+        }
+        return null;
+      },
+    );
+  }
 }
 
-class KeyPoint {
+class Point {
   String text = '';
   int seconds = 10;
 
-  KeyPoint(this.text, this.seconds);
+  Point(this.text, this.seconds);
 
-  factory KeyPoint.empty() => KeyPoint('', 10);
+  factory Point.empty() => Point('', 10);
 }
 
-enum KeyPointsState { adding, ready, presenting, stopped }
+class NotesScreen extends StatefulWidget {
+  static const name = 'notes';
+  NotesScreenState createState() => NotesScreenState();
+}
 
-class AppState extends State<App> with TickerProviderStateMixin {
-  List<KeyPoint> _keyPoints = [];
-  KeyPoint _newKeyPoint;
-  KeyPointsState _state = KeyPointsState.ready;
+class NotesScreenState extends State<NotesScreen> {
+  String notes;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes')
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onChanged: (value) => setState(() => notes = value),
+              maxLines: null,
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                    context,
+                    PresentationPage.routeName,
+                    arguments: notes
+                );
+              },
+              color: Colors.blue,
+              child: Text('Present'),
+            )
+          ],
+        )
+      )
+    );
+  }
+}
+
+enum PresentationState { ready, presenting, stopped }
+
+class PresentationPage extends StatefulWidget {
+  static const routeName = 'presentation';
+  final List<Point> _keyPoints;
+
+  PresentationPage(this._keyPoints);
+  PresentationPageState createState() => PresentationPageState();
+}
+
+class PresentationPageState extends State<PresentationPage> with TickerProviderStateMixin {
+  List<Point> _keyPoints;
+  PresentationState _state = PresentationState.ready;
 
   AnimationController _controller;
-
-  static const unitHeight = 60;
 
   @override
   void initState() {
     super.initState();
+    _keyPoints = widget._keyPoints;
     _controller = AnimationController(vsync: this);
   }
 
@@ -57,58 +125,130 @@ class AppState extends State<App> with TickerProviderStateMixin {
       _controller.addStatusListener((AnimationStatus status) {
         if (status == AnimationStatus.completed)
           setState(() {
-            _state = KeyPointsState.stopped;
+            _state = PresentationState.stopped;
           });
       });
       _controller.forward();
-      _state = KeyPointsState.presenting;
+      _state = PresentationState.presenting;
     });
   }
 
   int durationOfFirst(int index) =>
       _keyPoints.take(index).fold(0, (acc, keyPoint) => acc + keyPoint.seconds);
 
-  Widget keyPointBuild(int index, KeyPoint keyPoint) {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Presentation'),
+      ),
+      body: buildBody(),
+    );
+  }
+
+  Widget buildBody() {
+    return Column(
+        children: <Widget>[
+          headerBuild(),
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              children: keyPoints().toList(),
+            ),
+          ),
+        ]
+    );
+  }
+
+  Widget headerBuild() {
+    return Row(
+      children: <Widget>[
+        Spacer(),
+        AnimatedBuilder(
+            animation: _controller,
+            builder: (BuildContext context, Widget child) {
+              var duration = Duration(seconds: durationOfFirst(_keyPoints.length));
+              var elapsed = Duration(seconds: _controller.value.floor());
+              var remaining = duration - elapsed;
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '${remaining.inMinutes % 60}m ${remaining.inSeconds % 60}s',
+                  style: TextStyle(
+                      fontSize: 40
+                  ),
+                ),
+              );
+            }
+        ),
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.refresh),
+                iconSize: 30,
+                color: Colors.grey,
+                onPressed: () =>
+                    setState(() {
+                      _controller.reset();
+                      _state = PresentationState.ready;
+                    }),
+              ),
+              if (
+              _state == PresentationState.ready
+                  || _state == PresentationState.stopped)
+                IconButton(
+                    color: Colors.green,
+                    iconSize: 50,
+                    icon: Icon(Icons.play_circle_outline),
+                    onPressed: startPresentation
+                ),
+              if (_state == PresentationState.presenting)
+                IconButton(
+                  color: Colors.red,
+                  iconSize: 50,
+                  icon: Icon(Icons.pause_circle_outline),
+                  onPressed: () => setState(() {
+                    _state = PresentationState.stopped;
+                    _controller.stop();
+                  }),
+                ),
+            ],
+          )
+        ),
+      ],
+    );
+  }
+
+  Widget keyPointBuild(int index, Point keyPoint) {
+    const unitHeight = 60;
+
     var actionMenu =
         Row(
           children: <Widget>[
-            MaterialButton(
+            OutlineButton(
                 color: Colors.grey,
                 onPressed: () {
-                  if (_state != KeyPointsState.adding) {
-                    setState(() {
-                      var point = _keyPoints[index];
-                      if (point.seconds > 10) {
-                        point.seconds = point.seconds - 10;
-                        resetAnimationController();
-                      }
-                    });
-                  }
+                  setState(() {
+                    var point = _keyPoints[index];
+                    if (point.seconds > 10) {
+                      point.seconds = point.seconds - 10;
+                      resetAnimationController();
+                    }
+                  });
                 },
                 child: Text('-10s')
             ),
             Padding(padding: EdgeInsets.all(8)),
-            MaterialButton(
+            OutlineButton(
                 color: Colors.grey,
                 onPressed: () {
-                  if (_state != KeyPointsState.adding) {
-                    setState(() {
-                      _keyPoints[index].seconds = _keyPoints[index].seconds + 10;
-                      resetAnimationController();
-                    });
-                  }
+                  setState(() {
+                    _keyPoints[index].seconds = _keyPoints[index].seconds + 10;
+                    resetAnimationController();
+                  });
                 },
                 child: Text('+10s')
             ),
-            Padding(padding: EdgeInsets.all(8)),
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    _keyPoints.removeAt(index);
-                  });
-                },
-                icon: Icon(Icons.delete)
-            )
           ],
         );
 
@@ -122,7 +262,7 @@ class AppState extends State<App> with TickerProviderStateMixin {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Expanded(child: Text(keyPoint.text)),
-              if (_state == KeyPointsState.ready)
+              if (_state == PresentationState.ready)
                 actionMenu,
             ],
           ),
@@ -146,133 +286,13 @@ class AppState extends State<App> with TickerProviderStateMixin {
   Iterable<Widget> keyPoints() sync* {
     for (var index in Iterable.generate(_keyPoints.length)) {
       var keyPoint = _keyPoints[index];
-      yield GestureDetector(
+      yield Card(
         key: Key(index.toString()),
-        onTap: () => setState(() {
-          if (_state != KeyPointsState.adding) {
-            _keyPoints[index].seconds = _keyPoints[index].seconds + 10;
-            resetAnimationController();
-          }
-        }),
-        child: Card(
-          margin: EdgeInsets.all(5),
-          clipBehavior: Clip.hardEdge,
-          child: keyPointBuild(index, keyPoint),
-        ),
+        margin: EdgeInsets.all(5),
+        clipBehavior: Clip.hardEdge,
+        child: keyPointBuild(index, keyPoint),
       );
     }
   }
 
-  void saveNewKeyPoint() {
-    setState(() {
-      _keyPoints.add(_newKeyPoint);
-      _newKeyPoint = null;
-      _state = KeyPointsState.ready;
-      resetAnimationController();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Keypoint Timer',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-          appBar: AppBar(
-              title: Text(
-                'Key Points Timer',
-                style: TextStyle(),
-              )
-          ),
-          floatingActionButton: _state != KeyPointsState.ready
-              ? null
-              : FloatingActionButton(
-                  backgroundColor: Colors.blue,
-                  onPressed: () {
-                    setState(() {
-                      _newKeyPoint = KeyPoint.empty();
-                      _state = KeyPointsState.adding;
-                    });
-                  },
-                  child: Icon(Icons.add),
-                ),
-          body: Column(children: <Widget>[
-              AnimatedBuilder(
-                animation: _controller,
-                builder: (BuildContext context, Widget child) {
-                  var duration = Duration(seconds: durationOfFirst(_keyPoints.length));
-                  var elapsed = Duration(seconds: _controller.value.floor());
-                  var remaining = duration - elapsed;
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      '${remaining.inMinutes % 60}m ${remaining.inSeconds % 60}s',
-                      style: TextStyle(
-                        fontSize: 40
-                      ),
-                    ),
-                  );
-                }
-              ),
-              Expanded(
-                child: ReorderableListView(
-                    onReorder: (oldIndex, newIndex) {
-                      setState(() {
-                        _keyPoints.insert(newIndex, _keyPoints[oldIndex]);
-                        _keyPoints.removeAt(oldIndex);
-                      });
-                    },
-                    children: keyPoints().toList()),
-              ),
-              if (_state == KeyPointsState.adding)
-                TextFormField(
-                  key: Key(_keyPoints.length.toString()),
-                  initialValue: _newKeyPoint.text,
-                  onChanged: (newValue) =>
-                      setState(() => _newKeyPoint.text = newValue),
-                  onEditingComplete: saveNewKeyPoint,
-                  autofocus: true,
-                ),
-              if (_state == KeyPointsState.ready)
-                MaterialButton(
-                  color: Colors.green,
-                  child: Text('Start'),
-                  onPressed: startPresentation
-                ),
-              if (_state == KeyPointsState.presenting)
-                MaterialButton(
-                  color: Colors.red,
-                  child: Text('Stop'),
-                  onPressed: () => setState(() {
-                    _state = KeyPointsState.stopped;
-                    _controller.stop();
-                  }),
-                ),
-              if (_state == KeyPointsState.stopped)
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    MaterialButton(
-                        color: Colors.green,
-                        child: Text('Start'),
-                        onPressed: startPresentation
-                    ),
-                    Padding(padding: EdgeInsets.all(10)),
-                    MaterialButton(
-                        color: Colors.red,
-                        child: Text('Reset'),
-                        onPressed: () =>
-                            setState(() {
-                              _controller.reset();
-                              _state = KeyPointsState.ready;
-                            })
-                    )
-                  ]
-                ),
-            ]),
-          ),
-    );
-  }
 }
